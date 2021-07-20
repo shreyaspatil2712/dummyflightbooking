8749058b470542fc9e234aabf32b677c19faa8e5
shreyaspatil2712 <87693150+shreyaspatil2712@users.noreply.github.com>
2021-07-20 14:30:19 +0530

    Delete air directory

--- air/select.py
+++
@@ -1,5 +0,0 @@
-def select(board): # selecting the seat 
-    print("select desired column and a seat in that column")
-    x = int(input("Enter column number:" ))
-    y = int(input("Enter desried seat in the column:" ))
-    board[y][x] = "booked"
